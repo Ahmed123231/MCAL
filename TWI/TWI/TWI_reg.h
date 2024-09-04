c827d61aa15709e12e53e0b57e5b8d0bb9d0df48
Ahmed Atef <zarakikenbachi28@gmail.com>
2024-09-04 23:18:30 +0300

    v0.0

---
+++ TWI/TWI/TWI_reg.h
@@ -0,0 +1,22 @@
+/*
+ * TWI_reg.h
+ *
+ * Created: 7/5/2024 3:30:04 PM
+ *  Author: ahmed
+ */ 
+
+
+#ifndef TWI_REG_H_
+#define TWI_REG_H_
+
+#define  TWDR_REG     *((volatile u8*)0x23)
+#define  TWAR_REG     *((volatile u8*)0x22)
+#define  TWSR_REG     *((volatile u8*)0x21)
+#define  TWBR_REG     *((volatile u8*)0x20)
+#define  TWCR_REG     *((volatile u8*)0x56)
+
+
+
+
+
+#endif /* TWI_REG_H_ */
